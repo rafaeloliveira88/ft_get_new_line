c608090ac4628f355db43aeb2d078fa399f01645
Rafael <rafaeljmo88@gmail.com>
2024-11-20 09:08:26 +0000

    udate

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,13 +6,13 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 11:32:45 by rafael            #+#    #+#             */
-/*   Updated: 2024/11/20 00:13:28 by rafael           ###   ########.fr       */
+/*   Updated: 2024/11/20 09:07:52 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	getendposline(char *str)
+size_t	getlen(char *str)
 {
 	size_t	i;
 
@@ -44,28 +44,28 @@ size_t	ft_strlen(const char *s)
 char	*ft_joinmod(char *line, char *buf)
 {
 	char	*aux;
-	size_t	pos;
+	size_t	len_buf;
 	size_t	i;
 	size_t	len_line;
 
+	len_buf = getlen(buf);
 	len_line = ft_strlen(line);
-	aux = (char *)malloc(sizeof(char) * (len_line) + getendposline(buf) + 1);
+	aux = (char *)malloc(sizeof(char) * (len_line) + len_buf + 1);
 	if (!aux)
 		return (free(line), NULL);
 	i = -1;
 	while (++i < len_line)
 		aux[i] = line[i];
 	i--;
-	while (++i - len_line < getendposline(buf))
+	while (++i - len_line < len_buf)
 		aux[i] = buf[(i) - len_line];
 	if (buf[i - len_line] == '\n')
 		aux[i] = '\n';
-	aux[len_line + getendposline(buf)] = '\0';
-	pos = getendposline(buf);
+	aux[len_line + len_buf] = '\0';
 	i = -1;
-	while (++i < (ft_strlen(buf) - pos))
-		buf[i] = buf[pos + i];
-	buf[ft_strlen(buf) - pos] = '\0';
+	while (++i < (ft_strlen(buf) - len_buf))
+		buf[i] = buf[len_buf + i];
+	buf[ft_strlen(buf) - len_buf] = '\0';
 	buf[i] = '\0';
 	return (free(line), aux);
 }
